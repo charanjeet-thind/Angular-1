d51a696b949b3490dcccb9fca0101f5104d7c9e8
Anuj Sharma <anuj.sharma1@netsmartz.local>
2018-09-06 19:35:44 +0530

    Added functionality in role service

--- src/app/_services/roles.service.ts
+++ src/app/_services/roles.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { Role } from '../_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-  constructor() { }
+	constructor(private http: HttpClient) { }
+	getAll() {
+		return this.http.get<Role[]>(`${environment.apiUrl}/roles`);
+	}
+
+	getById(id: number) {
+		return this.http.get(`${environment.apiUrl}/roles/` + id);
+	}
+
+	register(role: Role) {
+		return this.http.post(`${environment.apiUrl}/roles/register`, role);
+	}
+
+	update(role: Role) {
+		return this.http.put(`${environment.apiUrl}/roles/` + role.id, role);
+	}
+
+	delete(id: number) {
+		return this.http.delete(`${environment.apiUrl}/roles/` + id);
+	}
 }
