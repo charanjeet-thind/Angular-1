9ba1acc152bda4b4f40bfa70d3a30fd5e2c22e70
Anuj Sharma <anuj.sharma1@netsmartz.local>
2018-09-06 18:33:55 +0530

    Added role creation module

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService } from '../_services';
 
 @Component({
-  selector: 'app-roles',
-  templateUrl: './roles.component.html',
-  styleUrls: ['./roles.component.css']
+  templateUrl: './roles.component.html'
 })
 export class RolesComponent implements OnInit {
+	roleForm: FormGroup;
+	loading = false;
+	submitted = false;
 
-  constructor() { }
+
+	constructor(
+	private formBuilder: FormBuilder,
+	private router: Router,
+	private userService: UserService,
+	private alertService: AlertService) { }
+
 
   ngOnInit() {
-  }
+        this.roleForm = this.formBuilder.group({
+            roleName: ['', Validators.required]
+        });
+    }
+	// convenience getter for easy access to form fields
+    get f() { return this.roleForm.controls; }
+	onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.roleForm.invalid) {
+            return;
+        }
+alert('here');
+        this.loading = true;
+        this.userService.register(this.roleForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 
 }
